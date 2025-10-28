6458a93e99a8971add3a365c4251d5e12bcce91d
D3ITY-YODA <brendanakudo@gmail.com>
2025-10-28 18:21:22 +0300

    Fix ALLOWED_HOSTS to use environment variable

--- store/settings/dev.py
+++ store/settings/dev.py
@@ -1,1 +1,14 @@
-from .base import *
+import os
+
+ALLOWED_HOSTS = [
+    'gamingstore-omf0.onrender.com',
+    '.onrender.com',
+    'localhost',
+    '127.0.0.1', 
+    '0.0.0.0',
+]
+
+# Also include any from environment variable
+env_hosts = os.environ.get('ALLOWED_HOSTS', '')
+if env_hosts:
+    ALLOWED_HOSTS.extend(env_hosts.split(','))
